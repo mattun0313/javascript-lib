71a6ba452c7a38f675218a62ce5bfbe19a53bbb5
Masanori Matsumoto <mattun.0313@gmail.com>
2012-03-07 16:51:48 +0900

    Japanese Holiday Add

--- jQuery/jquery.mCal.js
+++ jQuery/jquery.mCal.js
@@ -19,6 +19,7 @@
 //　MIT license.
 //
 // 	2012.03.06 	masanori.matsumoto	新規作成
+// 	2012.03.07 	masanori.matsumoto	祝日判定追加（2012/03/07現在）
 //================================================
 
 (function($){
@@ -30,7 +31,8 @@
 			day: new Date(),
 			weekTl:['日','月','火','水','木','金','土'],
 			weekCls:['sun','mon','tue','wed','thu','fri','sat'],
-			file:''
+			file:'',
+			jHolFlg:false/*japanese holiday*/
 		}
 		//初期設定をオプションのオブジェクトで上書き
 		opt = $.extend(defaultOpt, opt);
@@ -77,12 +79,18 @@
 		for(var i=0;i<arrCal.length;i++){
 			strHtml += '<tr>'
 			for(var j=0;j<7;j++){
-				//当日ならクラス名「today」付加
-				if(today.getFullYear()==opt.day.getFullYear()&&today.getMonth()==opt.day.getMonth()&&today.getDate()==arrCal[i][j]){
-					strHtml += '<th class="'+opt.weekCls[j]+' today'+'">' + arrCal[i][j] + '</th>'				
-				}else{
-					strHtml += '<th class="'+opt.weekCls[j]+'">' + arrCal[i][j] + '</th>'
+				var clsName = opt.weekCls[j];
+				if(arrCal[i][j]!='&nbsp;'){
+					//当日ならクラス名「today」付加
+					if(today.getFullYear()==opt.day.getFullYear()&&today.getMonth()==opt.day.getMonth()&&today.getDate()==arrCal[i][j]){
+						clsName += ' today';
+					}
+					//祝日判定
+					if(opt.jHolFlg&&jHoliday(opt.day.getFullYear(),opt.day.getMonth()+1,arrCal[i][j])){
+						clsName += ' jHol';
+					}
 				}
+				strHtml += '<th class="'+clsName+'">' + arrCal[i][j] + '</th>'				
 			}
 			strHtml += '</tr>'
 		}
@@ -102,4 +110,97 @@
 		});
 				
 	}
+	//-------------------祝日判定処理
+	function jHoliday(y,m,d){
+		var day = new Date(y,m-1,d).getDay();
+		var yesterDay;
+		
+		switch(m){
+			case 1:
+				if(d==1) return '元旦';
+				if(y>=2000){
+					if(day==1&&Math.ceil(d/7)==2) return '成人の日';
+				}else{
+					if(d==15) return '成人の日';
+				}
+				break;
+			case 2:
+				if(d==11) return '建国記念の日';
+				break;
+			case 3:
+				if(d==ssDay(y,0)) return '春分の日';
+				break;
+			case 4:
+				if(d==29){
+					if(y>=2007) return '昭和の日';
+					if(y>=1989) return 'みどりの日';
+					return '天皇誕生日';
+				}
+				break;
+			case 5:
+				if(d==3) return '憲法記念日';
+				if(d==4){
+					if(y>=2007) return 'みどりの日';
+					if(y>=1986&&day>1) return '国民の休日'; //日曜は日曜、月曜だと憲法記念日の振替
+				}
+				if(d==5) return 'こどもの日';
+				if(d==6){
+					if(y>=2007){
+						if(day==2||day==3) return '振替休日';
+					}
+				} 				
+				break;
+			case 6:
+				break;
+			case 7:
+				if(y>=2003){
+					if(day==1&&Math.ceil(d/7)==3) return '海の日';
+				}else{
+					if(y>=1996&&d==20) return '海の日';
+				}
+				break;
+			case 8:
+				break;
+			case 9:
+				if(d==ssDay(y,1)) return '秋分の日';
+				if(y>=2003){
+					if(day==1&&Math.ceil(d/7)==3) return '敬老の日';
+					if(day==2&&d==ssDay(y,1)-1) return '国民の休日';
+				}else{
+					if(y>=1966&&d==15) return '敬老の日';
+				}
+				break;
+			case 10:
+				if(y>=2000){
+					if(day==1&&Math.ceil(d/7)==2) return '体育の日';
+				}else{
+					if(y>=1966&&d==10) return '体育の日';
+				}
+				break;
+			case 11:
+				if(d==3) return '文化の日';
+				if(d==23) return '勤労感謝の日';
+				break;
+			case 12:
+				if(y>=1989&&d==23) return '天皇誕生日';
+				break;
+		}
+		//振替休日判定(月曜だけ)
+		if(day==1){
+			yesterDay = new Date(y,m-1,d-1);
+			return jHoliday(yesterDay.getFullYear(),yesterDay.getMonth()+1,yesterDay.getDate());
+		}
+		return ''
+	}
+	//春秋分の日計算-1980〜2100-（実際には官報で翌年の春分日を発表するので確定ではない）
+	function ssDay(y,kbn){
+		var baseNum; 
+		if(kbn==0){
+			baseNum = 20.8431;
+		}else{
+			baseNum = 23.2488;
+		}
+		return Math.floor(baseNum + 0.242194 * (y-1980)) - Math.floor((y-1980)/4);
+	}
+	
 })(jQuery);
