f2f8a91cbb91ede60e90f882b102413d79866447
Masanori Matsumoto <mattun.0313@gmail.com>
2012-03-27 18:27:31 +0900

    jquery.mSlShow.js update

--- jQuery/jquery.mSlShow.js
+++ jQuery/jquery.mSlShow.js
@@ -1,5 +1,5 @@
 //================================================
-//  jquery.mSlShow.js
+//  jquery.js
 //
 //  指定されたタグ内のそれぞれの子div要素をスライドショーさせるjQueryプラグインです。
 //
@@ -17,12 +17,8 @@
 //  });で実行
 //	option
 //		easing:fadeout easing
+//		time:show time
 //		du:fadeout time
-//
-//	注意）
-//	timerでは関数や変数のオブジェクトをバインドできない為、
-//	global(windowオブジェクト)にmSlShowオブジェクトを追加して対応しています。
-//	※他にいい方法があれば教えてください！
 //
 //  version 1.0.0
 //　MIT license.
@@ -31,14 +27,15 @@
 //================================================
 
 (function($){
-  window.mSlShow = {};
   $.fn.mSlShow = function(opt){
     var cnt = 1;
     var par = {};
+    var timId = 0;
 
     //初期設定
     var defaultOpt = {
       easing:'linear',
+      time:5000,
       du:3000
     }
     //
@@ -57,31 +54,33 @@
       }
     );
     
-    window.mSlShow.par = par;
-    
-    setTimeout('mSlShow.mSlShowFade()',3000);
-    
+	  var mSlShowFade = (function (){
+	  	return function(){
+	  		if(timId){
+			    par.targetChild.eq(par.curImg).animate(
+			      {opacity: 0},
+			      {duration: par.opt.du, easing: par.opt.easing,
+			        complete: function(){
+			          $(this).css({zIndex:0,opacity:1});
+			          $(this).siblings().each(
+			            function(){
+			              var zi = parseInt($(this).css('z-index'))+1;
+			              $(this).css('z-index',zi);
+			            }
+			          );
+			          par.curImg++;
+			          if(par.curImg>=par.max_cnt) par.curImg=0;
+						    timId = setTimeout(mSlShowFade,par.opt.time);
+			        }
+			      }
+			    );
+			  }else{
+			    timId = setTimeout(arguments.callee,par.opt.time);
+			  }
+		  }
+  	})();
+
+    mSlShowFade();
     return par.target;
   };
-
-  window.mSlShow.mSlShowFade = function (){
-    mSlShow.par.targetChild.eq(mSlShow.par.curImg).animate(
-      {opacity: 0},
-      {duration: mSlShow.par.opt.du, easing: mSlShow.par.opt.easing,
-        complete: function(){
-          $(this).css({zIndex:0,opacity:1});
-          $(this).siblings().each(
-            function(){
-              var zi = parseInt($(this).css('z-index'))+1;
-              $(this).css('z-index',zi);
-            }
-          );
-          mSlShow.par.curImg++;
-          if(mSlShow.par.curImg>=mSlShow.par.max_cnt) mSlShow.par.curImg=0;
-        }
-      }
-    );
-    setTimeout('mSlShow.mSlShowFade()',7000);
-  }
-
 })(jQuery);
